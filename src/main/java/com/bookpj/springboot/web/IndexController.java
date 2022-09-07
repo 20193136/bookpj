3df4b0eea39f1bf66ccc053fb6988be05de8e5e7
dldudco0310 <dldudco0310@inogard.co.kr>
2022-09-07 13:26:41 +0900

    테스트

--- src/main/java/com/bookpj/springboot/web/IndexController.java
+++ src/main/java/com/bookpj/springboot/web/IndexController.java
@@ -3,21 +3,30 @@ package com.bookpj.springboot.web;
 
 import com.bookpj.springboot.service.posts.PostsService;
 import com.bookpj.springboot.web.dto.PostsResponseDto;
+import com.bookpj.springboot.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user =(SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+            System.out.println(model);
+        }
         return "index";
     }
 
@@ -33,4 +42,6 @@ public class IndexController {
 
         return "posts-update";
     }
+
+
 }
