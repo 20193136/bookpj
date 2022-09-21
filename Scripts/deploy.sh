5dc07661ad95c2ecaed2f1f3edd4e0ce7f70dc46
dldudco0310 <dldudco0310@inogard.co.kr>
2022-09-21 14:05:33 +0900

    스크립트 수정

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -3,9 +3,9 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=bookpj
 
-echo >" 빌드 파일 복사"
+echo "> 빌드 파일 복사"
 
-cp REPOSITORY/zip/build/libs/*.jar REPOSITORY
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
@@ -35,4 +35,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring/profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>1& &
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
